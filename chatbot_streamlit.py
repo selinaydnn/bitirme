66501a532dde5cff44f9da213d93f1d44dab9c17
selinaydnn <selinaydin@live.com>
2025-04-12 17:24:16 +0300

    .

--- chatbot_streamlit.py
+++ chatbot_streamlit.py
@@ -1,3 +1,11 @@
+import sys
+import pysqlite3
+
+sys.modules["sqlite3"] = pysqlite3
+
+# Diğer importlarınız buraya
+
+
 import streamlit as st
 from utils import load_chroma_db, load_llm, create_qa_chain, create_prompt_template
 
